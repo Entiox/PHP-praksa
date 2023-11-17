3de844cbf97eab56e6be238330fc5292849ee4f2
Entiox <drazenantunovic012@gmail.com>
2023-11-17 01:33:03 +0100

    Added methods for handling requests to routes with parameters in controller

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -22,4 +22,37 @@ class IndexController
     {
         return new HtmlResponse(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams())));
     }
+
+    public static function indexActionWithId(Request $request, $id): Response
+    {
+        return new Response(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams(), "url parameters" =>  array("id" => $id))));
+    }
+
+    public static function indexJsonActionWithId(Request $request, $id): JsonResponse
+    {
+        return new JsonResponse(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams(), "url parameters" =>  array("id" => $id))));
+    }
+
+    public static function indexHtmlActionWithId(Request $request, $id): HtmlResponse
+    {
+        return new HtmlResponse(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams(), "url parameters" => array("id" => $id))));
+    }
+
+    public static function indexActionWithIds(Request $request, $firstId, $secondId): Response
+    {
+        return new Response(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams(),
+            "url parameters" => array("firstId" => $firstId, "secondId" => $secondId))));
+    }
+
+    public static function indexJsonActionWithIds(Request $request, $firstId, $secondId): JsonResponse
+    {
+        return new JsonResponse(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams(),
+            "url parameters" => array("firstId" => $firstId, "secondId" => $secondId))));
+    }
+
+    public static function indexHtmlActionWithIds(Request $request, $firstId, $secondId): HtmlResponse
+    {
+        return new HtmlResponse(array("result" => array("request url" => $request->getUrl(), "parameters" => $request->getParams(),
+            "url parameters" => array("firstId" => $firstId, "secondId" => $secondId))));
+    }
 }
--- src/Routing/Routes.php
+++ src/Routing/Routes.php
@@ -8,5 +8,5 @@ Route::get("/route1", [IndexController::class, "indexAction"]);
 Route::get("/route2", [IndexController::class, "indexJsonAction"]);
 Route::get("/route3", [IndexController::class, "indexHtmlAction"]);
 
-Route::get("/route4/{id}", [IndexController::class, "indexJsonAction"]);
-Route::get("/route5/{firstId}/{secondId}", [IndexController::class, "indexJsonAction"]);
+Route::get("/route4/{id}", [IndexController::class, "indexJsonActionWithId"]);
+Route::get("/route5/{firstId}/{secondId}", [IndexController::class, "indexHtmlActionWithIds"]);
